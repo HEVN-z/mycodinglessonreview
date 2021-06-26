da70fac67ed82a27f5d3375aa464882d9a92c8e1
HEAVENz Striker <44270716+heaven13exe@users.noreply.github.com>
2021-06-27 04:57:17 +0700

    Update t16.py

--- python/t16.py
+++ python/t16.py
@@ -1187,3 +1187,34 @@ print(x)
 
 # =====================================================================
 
+#
+# startswith()
+#
+# Python String startswith() Method
+# Check if the string stats with "Hello":
+
+txt = "Hello, welcome to my world."
+x = txt.startswith("Hello")
+print(txt + " startswith(\"Hello\")")
+print(x)
+
+# Definition and Usage
+# The startswith() method returns True if the string starts with the specified value, otherwise False.
+# Syntax
+# string.startswith(value, start, end)
+
+# Parameter Values
+# value     Required. The value to check if the string starts with
+# start     Optional. An Integer specifying at which position to start the search
+# end       Optional. An Integer specifying at which position to end the search
+
+# More Examples
+# Check if position 7 to 20 starts with the charcters "wel":
+
+txt = "Hello, welcome to my world."
+x = txt.startswith("wel", 7, 20)
+print(txt, " startswith(\"wel\", 7, 20)")
+print(x)
+
+# =====================================================================
+
