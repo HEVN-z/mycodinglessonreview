a5dc277da7e026e5b059342fc89d0267b71247a1
HEAVENz Striker <44270716+heaven13exe@users.noreply.github.com>
2021-06-27 06:01:56 +0700

    Update t16.py

--- python/t16.py
+++ python/t16.py
@@ -1303,3 +1303,52 @@ print(x)
 #
 # translate()
 #
+# Python String translate() Method
+# Replace any "S" characters with a "P" character:
+# use a dictionary with ascii codes to replace 83 (S) with 80 (P):
+
+mydict = {83:80}
+txt = "Hello Sam!"
+print(txt, "translate(mydict)")
+print(txt.translate(mydict))
+
+# Definition and Usage
+# The translate() method returns a string where some specified characters are replaced with the character described in a dictionary, or in a mapping table.
+# Use the maketrans() method to create a mapping table.
+# If a character is not specified in the dictionart/table, the character will not be replaed.
+# If you use a dictionary, you must use ascii codes instead of characters.
+# Syntax
+# string.translate(table)
+
+# More Examples
+# Use a mapping table to repllace "S" with "P":
+
+txt = "Hello Sam!"
+mytable = txt.maketrans("S", "P")
+print(txt, " translate(mytable)")
+print(txt.translate(mytable))
+
+# Example
+# The third parameter in the mapping table describes characters that you want to remove from the string:
+
+txt = "Good night Sam!"
+x = "mSa"
+y = "eJo"
+z = "odnght"
+mytable = txt.maketrans(x, y, z)
+print(txt, "maketrans(x, y, z)")
+print(x)
+
+# Example
+# The same example as above, but using a dictionary instead of a mapping table:
+
+txt = "Good night Sam!"
+mydict = {109:101,83:74,97:111,111:None,100:None,110:None,103:None,104:None,116:None}
+print(txt, " translate(mydict)")
+print(txt.translate(mydict))
+
+# ======================================================================
+
+#
+#
+#
