812b41ddb321960e70a9a85dc67b6296ce97ac6a
HEAVENz Striker <44270716+heaven13exe@users.noreply.github.com>
2021-06-27 06:15:21 +0700

    Update t16.py

--- python/t16.py
+++ python/t16.py
@@ -1369,5 +1369,36 @@ print(x)
 # =====================================================================
 
 #
-# 
+# zfill()
 #
+# Python String zfill() Method
+# Fill the string with zeros until it is 10 characters long:
+
+txt = "50"
+x = txt.zfill(10)
+print(txt, "zfill(10)")
+print(x)
+
+# Definition and Usage
+# The zfill() method adds zeros(0) at beginning of the string, until it reaches the specified length.
+# If the value of the len parameter is less than the length of the string, no filling is done.
+# Syntax
+# string.zfill(len)
+
+# Parameter Values
+# len       Required. A number specifying the desired length of the string
+
+# More Examples
+# Fill the strings with zeros until they are 10 characters long:
+
+a = "hello"
+b = "welcome to the jungle"
+c = "10,000"
+
+print(a, "zfill(10)")
+print(a.zfill(10))
+print(b, "zfill(10)")
+print(b.zfill(10))
+print(c, "zfill(10)")
+print(c.zfill(10))
+
