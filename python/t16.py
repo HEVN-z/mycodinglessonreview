41428942430a6363a755ac685ae8dcce22471e04
HEAVENz Striker <44270716+heaven13exe@users.noreply.github.com>
2021-06-27 05:39:15 +0700

    Update t16.py

--- python/t16.py
+++ python/t16.py
@@ -1265,3 +1265,41 @@ print(x)
 
 # =================================================================
 
+#
+# title()
+#
+# Python String title() Method
+# Make the first letter in each word upper case:
+
+txt = "Welcome to my world"
+x = txt.title()
+print(txt, " title()")
+print(x)
+
+# Definition and Usage
+# The title() method returns a string where the first character in every word in upper case. Like a header, or a title.
+# If the word contains a number or a symbol, the first letter after that will be converted to upper case.
+# Syntax
+# string.title()
+
+# More Examples
+# Make the first letter in each word upper case:
+
+txt = "Welcome to my 2nd world"
+x = txt.title()
+print(txt, " tilte()")
+print(x)
+
+# Example
+# Note that the first letter after a non-alphabet letter is converted into a case letter:
+
+txt = "hello b2b2b2 and 3g3g3g"
+x = txt.title()
+print(txt, " title()")
+print(x)
+
+#===================================================================
+
+#
+# translate()
+#
