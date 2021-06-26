cfa102d1d93d4fa9c8ce2a30667d6a8cd3cbff89
HEAVENz Striker <44270716+heaven13exe@users.noreply.github.com>
2021-06-27 05:10:07 +0700

    Update t16.py

--- python/t16.py
+++ python/t16.py
@@ -1247,3 +1247,21 @@ print(x)
 
 # ==============================================================
 
+#
+# swapcase()
+#
+# Python String swapcase() Method
+# Make the lower case letters upper case and the upper case letters lower case:
+
+txt = "Hello My Name Is PETTER"
+x = txt.swapcase()
+print(txt, " swapcase()")
+print(x)
+
+# Definition and Usage
+# The swapcase() method returns a string where all the upper case letters are lower case and vice versa.
+# Syntax
+# string.swapcase()
+
+# =================================================================
+
