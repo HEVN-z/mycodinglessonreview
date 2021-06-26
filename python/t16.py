5d659595141c36aed0de880f2ddd5515912b5eb4
HEAVENz Striker <44270716+heaven13exe@users.noreply.github.com>
2021-06-26 18:10:26 +0700

    Update t16.py

--- python/t16.py
+++ python/t16.py
@@ -1083,5 +1083,34 @@ print(x)
 # ========================================================================
 
 #
+# rstrip()
 #
+# Python String rstrip() Method
+# Remove any whitespace at the end of the string:
+
+txt = "      banana     "
+x = txt.rstrip()
+print(txt," rstrip()")
+print("of all fruits", x, "is my favorite")
+
+# Definition and Usage
+# The rstrip() method removes any trailing characters (characters at the end a string), space is the default trailing character to remove.
+# Syntax
+# string.rstrip(characters)
+
+# Parameter Values
+# characters    Optional. A set of characters to remove as trailing characters
+
+# More Examples
+# Remove the trailing characters if they are commas, s, q, or w:
+
+txt = "banana,,,,,ssqqww....."
+x = txt.rstrip(",.qsw")
+print(txt, "rstrip(\",.qsw\")")
+print(x)
+
+# ==============================================================
+
+#
+# 
 #
