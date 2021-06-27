935f6f2dfd3e8330c1b275c49b264c64733fe32a
HEAVENz Striker <44270716+heaven13exe@users.noreply.github.com>
2021-06-27 07:31:14 +0700

    Update

---
+++ python/t18.py
@@ -0,0 +1,101 @@
+# Python Operators
+print("10 + 5")
+print(10 + 5)
+
+# Python Arithmetic Operators
+# Arithmetic operators are used with numeric values perform common mathematical operations:
+
+print("x + y Addition")
+print("10 + 5")
+print(10 + 5)
+
+print("x - y Substraction")
+print("10 - 5")
+print(10 - 5)
+
+print("x * y Multiplication")
+print("10 * 5")
+print(10 * 5)
+
+print("x / y Diviision")
+print("10 / 5")
+print(10 / 5)
+
+print("x % y Modulus")
+print("10 % 5")
+print(10 % 5)
+
+print("x ** y Exponentiation")
+print("10 ** 5")
+print(10 ** 5)
+
+print("x // y Floor division")
+print("10 // 5")
+print(10 // 5)
+
+# Python Assignment Operators
+# Operator      Example     Same As
+#   =           x = 5       x = 5
+#   +=          x += 3      x = x + 3
+#   -=          x -= 3      x = x - 3
+#   *=          x *= 3      x = x * 3
+#   /=          x /= 3      x = x / 3
+#   %=          x %= 3      x = x % 3
+#   //=         x //= 3     x = x // 3
+#   **=         x **= 3     x = x ** 3
+#   &=          x &= 3      x = x & 3
+#   |=          x |= 3      x = x | 3
+#   ^=          x ^= 3      x = x ^ 3
+#   >>=         x >>= 3     x = x >> 3
+#   <<=         x <<= 3     x = x << 3
+
+
+# Python Comparison Operators
+# Compareison operators are used to compare two value:
+# Operator      Name                            Example
+# ==            Equal                           x == y
+# !=            Not equal                       x != y
+# >             Greater than                    x > y
+# <             Less than                       x < y
+# >=            Greater than or equal to        x >= y
+# <=            Less than or equal to           x <= y
+
+
+
+# Python Logical Operators
+# Logical Operators are used to combine conditional statements:
+# Operator      Description                     Example
+
+# and           Return True if both             x < 5 and x < 10
+#               statements are true
+
+# or            Return True if one of           x < 5 or x < 4
+#               the statement is true
+
+# not           Reverse the result, returns     not(x < 5 and x < 10)
+#               False if the result is true
+
+
+# Python Identity Operators
+# Identity operators are used to compare the objects, not if they are equal, but if they are actually the same object, with the same memory location:
+# Operator      Description                     Example
+
+# is            Return True if both variables   x is y
+#               are the same object
+
+# is not        Return True if both variables   x is not y
+#               are not the same object
+
+
+# Python Membership Operators
+# Membership operators are used to test if a sequence is presented in an object:
+# Operator      Description                     Example
+
+# in            Return True if a sequence       x in y
+#               with the specified value is
+#               present in the object
+
+# not in        Return True if a sequence       x not in y
+#               with the specified value is
+#               NOT present in the object
+
