9467f70bc9ab564bff37b974c5a3791d1dde5288
HEAVENz Striker <44270716+heaven13exe@users.noreply.github.com>
2021-07-07 18:28:43 +0700

    Update t19.py

--- python/t19.py
+++ python/t19.py
@@ -346,3 +346,61 @@ print(thislist)
 
 # ['apple', 'cherry']
 
+# 
+# Remove Specified Index
+#
+# Example
+# Remove the second item:
+
+thislist = ["apple","banana","cherry"]
+thislist.pop(1)
+print(thislist)
+
+# ['apple', 'cherry']
+
+# If you do not specify the index, the pop() method remove the last item.
+
+# Example
+# Remove the last item:
+
+thislist = ["apple","banana","cherry"]
+thislist.pop()
+print(thislist)
+
+# ['apple', 'banana']
+
+# The del keyword also removes the specified indes:
+
+# Example 
+# Remove the first item:
+
+thislist = ["apple","banana","cherry"]
+del thislist[0]
+print(thislist)
+
+# ['banana', 'cherry']
+
+# The del keyword can also delete the list completely.
+
+# Example
+# Delete the entire list:
+
+thislist = ["apple","banana","cherry"]
+del thislist
+print(thislist)
+
+# NameError: name 'thislist' is not defined
+
+#
+# Clear the List
+#
+# The clear() method empties the list.
+# The list still remains, but it has no content.
+
+# Example
+# Clear the list content:
+
+thislist = ["apple","banana","cherry"]
+thislist.clear()
+print(thislist)
+
