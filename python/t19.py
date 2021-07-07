9ad4378216f99fab2c8290bf50da3583000cf41b
HEAVENz Striker <44270716+heaven13exe@users.noreply.github.com>
2021-07-07 17:06:19 +0700

    Update t19.py

--- python/t19.py
+++ python/t19.py
@@ -1,1 +1,34 @@
 # Python Lists
+
+mylist = ["apple","banana","cherry"]
+
+# List
+# List are used to store multiple items in a single variable.
+# Liist are one of 4 built-in data types in Python used to store collections of data, the other 3 are Tuple, Set and Dictionary, all with different qualities and usage.
+# List are create using square brackets:
+
+# Example
+# Create a List:
+
+thislist = ["apple", "banana","cherry"]
+print(thislist)
+
+# List Items
+# List items are ordered, changeable, and allow duplicate value.
+# List items are indexed, the first item has index [0], the second item has index [1] etc.
+
+# Ordered
+# When we say that lists are ordered, it means that the items have a defined order, and that order will not change.
+# If you add new items to a list, the new items will be placed at the end of the list.
+
+# Changeable
+# The list is changeable, meaning that we can change, add, and remove items in list after it has been created.
+
+# Allow Duplicates
+# Sinc lists are indexed, Lists can have items with the same value:
+
+# Example
+# List allow duplicate value:
+
+thislist = ["apple","banana","cherry","apple","cherry"]
+print(thislist)
