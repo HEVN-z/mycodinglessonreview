c721e70ac0cef66f7e7c085f0f1c2b30702bf4fe
HEAVENz Striker <44270716+heaven13exe@users.noreply.github.com>
2021-07-09 06:47:37 +0700

    Update t21.py

--- python/t21.py
+++ python/t21.py
@@ -8,4 +8,14 @@ myset = {"apple","banana","cherry"}
 # Set
 #
 # Sets are used to store multiple items in a single variable.
-# Set is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Tuple, and Dictionary, all with qualities
+# Set is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Tuple, and Dictionary, all with qualities and usage.
+# A set is a collection which is both unordered and unindexed.
+# Sets are written with curly brackets.
+
+# Example
+# Create a Set:
+
+thisset = {"apple","banana","cherry"}
+print(thisset)
+# {'cherry', 'apple', 'banana'}
+
