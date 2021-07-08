32f2adb2c7f6d984f3b5d1bf81aefe0c30ea86ba
HEAVENz Striker <44270716+heaven13exe@users.noreply.github.com>
2021-07-09 02:12:50 +0700

    Update t21.py

---
+++ python/t21.py
@@ -0,0 +1,11 @@
+#
+# Python - Sets
+#
+
+myset = {"apple","banana","cherry"}
+
+#
+# Set
+#
+# Sets are used to store multiple items in a single variable.
+# Set is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Tuple, and Dictionary, all with qualities
