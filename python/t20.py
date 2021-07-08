9f0b8bb599a040fcacc8ca122b9ba970e56458ac
HEAVENz Striker <44270716+heaven13exe@users.noreply.github.com>
2021-07-08 20:02:17 +0700

    Update t20.py

--- python/t20.py
+++ python/t20.py
@@ -414,3 +414,43 @@ while i < len(thistuple):
 # =============================================================================
 
 #
+# Python - Join Tuples
+#
+
+# Join Two Tuples
+# To join two or more tuples you can use the + operator:
+
+# Example
+# Join two tuples:
+
+tuple1 = ("a","b","c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+# ('a', 'b', 'c', 1, 2, 3)
+
+# 
+# Multiply Tuples
+#
+# If you want to multiply the content of a tuple of a given number of times, you can use the * operator:
+
+# Example
+# Multiply the fruits tuple by 2:
+
+fruits = ("apple","banana","cherry")
+mytuple = fruits * 2
+print(mytuple)
+
+# ===============================================================================
+
+#
+# Python - Tuple Methods
+#
+
+# Tuple Methods
+# Python has two built-in methods that you can use on tuples.
+
+# Method        Descruption
+# count()       Returns the number of times a specifieid value occurs in a tuple
+# index()       Searches the tuple for a specified value and returns the position of where it was found
