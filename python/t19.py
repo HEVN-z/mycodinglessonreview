6390c5a7969752043a4d5e1bdf9679a5c2d83867
HEAVENz Striker <44270716+heaven13exe@users.noreply.github.com>
2021-07-08 18:38:31 +0700

    get a new lession

--- python/t19.py
+++ python/t19.py
@@ -753,4 +753,48 @@ list2 = [1,2,3]
 list3 = list1 + list2
 print(list3)            # ['a', 'b', 'c', 1, 2, 3]
 
+# Another way to join two lists is by appending all the items from list2 into list1, one by one:
+
+# Example
+# Append list2 into list1:
+
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+for x in list2:
+    list1.append(x)
+print(list1)
+# ['a', 'b', 'c', 1, 2, 3]
+
+# Or you can use the extend() method, which purpose is to add elements from one list to another list:
+
+# Example
+# Use the extend() method to add list2 at the end of list1:
+
+list1 = ["a","b","c"]
+list2 = [1, 2, 3]
+list1.extend(list2)
+print(list1)
+# ['a', 'b', 'c', 1, 2, 3]
+
+# ============================================================================
+
 #
+# Python - List Methods
+#
+
+# List Methods
+# Python has a set of built-in methods that you can use on lists.
+
+# Method    Description
+# append()  Adds an element at the end of list
+# clear()   Removes all the elements from the list
+# copy()    Returns a copy of the list
+# count()   Returns the number of elements with the specified value
+# extend()  Add the elements of a list (or any iterable), to the end of the current list
+# index()   Returns the index of the first element with the specified value
+# insert()  Adds an element at the specified position
+# pop()     Removes the element at the specified position
+# remove()  Removes the item with the specified value
+# reverse() Reverse the order of the list
+# sort()    Sorts the list
+
---
+++ python/t20.py
@@ -0,0 +1,21 @@
+#
+# Python Tuples
+#
+
+mytuple = ("apple", "banana", "cherry")
+
+#
+# Tuple
+#
+# Tuple are used to store multiple items in a single variable.
+# Tuple is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Set, and Dictionart, all with different qualities and usage.
+# A tuple is collection which is ordered and unchangeable.
+# Tuple are written with round brackets.
+
+# Example
+# Create a Tuple:
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+# ('apple', 'banana', 'cherry')
+
