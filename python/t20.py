da5a304f2dd36e897197fb6d7cab49ca48dec07f
HEAVENz Striker <44270716+heaven13exe@users.noreply.github.com>
2021-07-08 19:36:25 +0700

    Update t20.py

--- python/t20.py
+++ python/t20.py
@@ -221,5 +221,88 @@ if "apple" in thistuple:
 # ===========================================================================
 
 #
+# Python - Update Tuples
+#
+
+# Tuple are unchangeable, meaning that you cannot change, add, or remove items once the tuple is created.
+# But there are some workarounds.
+
+#
+# Change Tuple Values
+#
+# Once a tuple is crated, you cannot change its values. Tuple are unchanveable, or immutable as it also is called.
+# But there is a workaround. You can convert the tuple into a list, change the list, and convert the list back into a tuple.
+
+# Example
+# Convert the tuple into a list to be able to change it:
+
+x = ("apple","banana","cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+print(x)
+# ('apple', 'kiwi', 'cherry')
+
+#
+# Add Items
+# Sinc tuple are immutable, they do not have a build-in append() method, but there are other ways to add items to a tuple.
+
+# 1. Convert into a list: Just like the workaround for changeing a tuple, you can convert it into a list, add your item(s), and convert it back into a tuple.
+
+# Example
+# Convert the tuple into a list, add "orange", and convert it back into a tuple:
+
+thistuple = ("apple","banana","cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+print(thistuple)            # ('apple', 'banana', 'cherry', 'orange')
+print(type(thistuple))      # <class 'tuple'>
+
+# 2. Add tuple to a tuple. You are allowed to add tuples to tuples, so if you want to add one item,(or many), create a new tuple with the item(s), and add it to the exist tuple:
+
+# Example
+# Create a new tuple with the value "orange", and add that tuple:
+
+thistuple = ("apple","banana","cherry")
+y = ("orange",)
+thistuple += y
+print(thistuple)
+# ('apple', 'banana', 'cherry', 'orange')
+thistuple += ("strawberry",)
+print(thistuple)
+# ('apple', 'banana', 'cherry', 'orange', 'strawberry')
+
+## NOTE: When creating a tuple with only one item, remember to include a comma after the item, otherwise it will not be identified as a tuple.
+
+#
+# Remove Items
+#
+# NOTE: You cannot remove items in a tuple.
+# Tuple are unchangeable, so you cannot remove items from it, but you can use the same workaround as we used for changing and adding tuple items.
+
+# Example
+# Convert the tuple into a list, remove "apple", and convert it back into a tuple:
+
+thistuple = ("apple","banana","cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+print(thistuple)
+# ('banana', 'cherry')
+
+# Or you can delete the tuple completely:
+
+# Example
+# The del keyword can delete the tuple completely:
+
+thistuple = ("apple","banana","cherry")
+del thistuple
+print(thistuple)
+# NameError: name 'thistuple' is not defined
+
+# ==============================================================================
+
+#
 # 
 #
