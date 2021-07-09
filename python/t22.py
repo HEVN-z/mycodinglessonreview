bfa26a79157c28133b4cfb246e67c662c3f4b903
HEAVENz Striker <44270716+heaven13exe@users.noreply.github.com>
2021-07-09 17:36:05 +0700

    Update t22.py

--- python/t22.py
+++ python/t22.py
@@ -305,4 +305,139 @@ if "model" in thisdict:
 #
 
 # Change Values
-# 
+# You can change the value of a specific item by referring to its key name:
+
+# Example
+# Change the "year" to 2018:
+
+thisdict = {
+    "brand":"Ford",
+    "model":"Mustang",
+    "year" :1964
+}
+thisdict["year"] = 2018
+print(thisdict)
+# {'brand': 'Ford', 'model': 'Mustang', 'year': 2018}
+
+#
+# Update Dictionary
+# The update() method will update the dictionary with the items from the given argument.
+# The argument must be a dictionary, or an iterable object with key:value pairs.
+
+# Example
+# Update the "year" of the car by using the update() method:
+
+thisdict = {
+    "brand":"Ford",
+    "model":"Mustang",
+    "year" :1964
+}
+thisdict.update({"year":2020})
+print(thisdict)
+# {'brand': 'Ford', 'model': 'Mustang', 'year': 2020}
+
+# ================================================================================
+
+#
+# Python - Add Dictionary Items
+#
+
+# Adding Items
+# Adding an item to the dictionary is done by using a new index key and assigning a value to it:
+
+# Example
+
+thisdict = {
+    "brand":"Ford",
+    "model":"Mustang",
+    "year" :1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+# {'brand': 'Ford', 'model': 'Mustang', 'year': 1964, 'color': 'red'}
+
+#
+# Update Dictionary
+# The update() method will update dictionary with the items from a given argument. If the item does not exist, the item will be added.
+# The argument must be a dictionary, or an iterable object with key:value pairs.
+
+# Example
+# Add a color item to the dictionary by using the update() method:
+
+thisdict = {
+    "brand":"Ford",
+    "model":"Mustang",
+    "year" :1964
+}
+thisdict.update({"color":"red"})
+print(thisdict)
+# {'brand': 'Ford', 'model': 'Mustang', 'year': 1964, 'color': 'red'}
+
+# =================================================================================
+
+#
+# Python - Remove Dictionary Items
+#
+
+# Removing Items
+# There are several methods to remove items from a dictionary:
+
+# Example
+# The pop() method removes the item with the specified key name:
+
+thisdict = {
+    "brand":"Ford",
+    "model":"Mustang",
+    "year" :1964
+}
+thisdict.pop("model")
+print(thisdict)
+# {'brand': 'Ford', 'year': 1964}
+
+# Example
+# The popitem() method removes the last inserted item(in version before 3.7, a random item is removed insteaad):
+
+thisdict = {
+    "brand":"Ford",
+    "model":"Mustang",
+    "year" :1964
+}
+thisdict.popitem()
+print(thisdict)
+# {'brand': 'Ford', 'model': 'Mustang'}
+
+# Example
+# The del keyword temoves the item with the specified key name:
+
+thisdict = {
+    "brand":"Ford",
+    "model":"Mustang",
+    "year" :1964
+}
+
+# Example
+# The del keyword can also delete the dictionary completely:
+
+thisdict = {
+    "brand":"Ford",
+    "model":"Mustang",
+    "year" :1964
+}
+del thisdict
+# print(thisdict)
+# NameError: name 'thisdict' is not defined
+
+# Example
+# The clear() method empties the dictionary:
+
+thisdict = {
+    "brand":"Ford",
+    "model":"Mustang",
+    "year" :1964
+}
+thisdict.clear()
+print(thisdict)
+# {}
+
+# ==============================================================================
+
