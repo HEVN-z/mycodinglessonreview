c8c0774b6ca1a024b751fe80183db6b3f104ef65
HEAVENz Striker <44270716+heaven13exe@users.noreply.github.com>
2021-07-10 19:40:37 +0700

    Update t23.py

--- python/t23.py
+++ python/t23.py
@@ -122,4 +122,45 @@ print("A") if a > b else print("=") if a == b else print("B")
 
 a = 200
 b = 33
+c = 500
+if a > b and c > a:
+    print("Both condition are True")
 
+#
+# Or
+# The or keyword is a logical operator, and to combine conditional statements:
+
+# Example
+# Test if a is greater than b, OR if a is greater than c:
+
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+    print("At least one of the condition is True")
+
+#
+# Nested If
+# You can have if statement inside if statements, this is called nested if statements.
+
+# Example
+
+x = 41
+if x > 10:
+    print("Above ten,")
+    if x > 20:
+        print("and also above 20!")
+    else:
+        print("But not above 20.")
+
+#
+# The pass Statement
+# if statements cannot be empty, but if you for some reason have an if statement with no content, put in the pass statement to avoid getting an error.
+
+# Example
+
+a = 33
+b = 200
+if b > a:
+    pass
+# 
