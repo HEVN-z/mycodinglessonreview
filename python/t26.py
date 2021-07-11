7de7d4bfbc08c861a52f7cc8610ab3d2c19d8e78
HEAVENz Striker <44270716+heaven13exe@users.noreply.github.com>
2021-07-11 16:39:30 +0700

    0

--- python/t26.py
+++ python/t26.py
@@ -64,4 +64,114 @@ my_function("Emil","Refsnes")
 
 # If you try to call the function with 1 or 3 arguments, you will get an error:
 
+#
+# Arbitary Arguments, *args
+# If you do not know how many arguments that will be passed into your function, add a * before the parameter name in the function definition.
+# This wat the function will recieve a tuple of arguments, and can access the items accordingly:
 
+# Example
+# If the number of arguments is unknown, add a * before the parameter name:
+
+def my_function(*kids):
+    print("The youngest child is " + kids[2])
+my_function("Emil","Tobias","Linus")
+
+## Arbitrary Arguments are often shorted to *args in Pythom documents.
+
+#
+# Keyword Arguments
+# You can also send arguments with the key = value syntax.
+# This way the order of the arguments does not matter.
+
+# Example
+def my_function(child3,child2, child1):
+    print("The youngest child is " + child3)
+my_function(child1 = "Emil", child2 = "Tobia", child3 = "Linus")
+
+## The phrase Keyword Arguments are often shorten shorted to kwargs in Python documents.
+
+#
+# Arbirary Keyword Arguments, **kwargs
+# If you do not know how many keyword arguments that will be passed into function, add two asterisk: ** before the parameter name in the function define.
+# This way the function will recieve a dictionary of arguments, and can access the items accordingly:
+
+# Example
+# If the number of keyword arguments is unknown, add a double ** before the parameter name:
+
+def my_function(**kid):
+    print("His last name is " + kid["lname"])
+
+my_function(fname = "Tobia", lname = "Refsnes")
+
+## Arbitrary Keyword Arguments are often shortened to **kwargs in Python
+
+#
+# Default Parameter Value
+# The following example shows how to use a default parameter value.
+# If we call the function without argument, it uses the default value:
+
+# Example
+
+def my_function(country = "Norway"):
+    print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+
+#
+# Passing a List as Argument
+# You can send any data types of argument to a function (string, number, list, dictionary etc.),
+# E.g. if you send a List as an argument, it will still be a List when it reaches the function:
+
+# Example
+
+def my_function(food):
+    for x in food:
+        print(x)
+fruits = ["apple","banana","cherry"]
+
+my_function(fruits)
+
+#
+# Return Values
+# To let a function return a value, usd the return statement:
+
+# Example
+
+def my_function(x):
+    return 5 * x
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+
+#
+# The pass Statement
+# Function definitions cannot be empty, but if you for some reason have a function definition with no contentm put in the pass statement to avoid getting an error.
+
+# Example
+
+def myfunction():
+    pass
+
+#
+# Recursion
+# Python also accepts function recursion, which means a defined function can call itself.
+# Recursion is common mathematical and programming concept. It means that a function calls itself. This has the benefit of meaning that you can loop through data to reach a result.
+# The developer should be very careful with recursion as it can be quite easy to slip into writing a function which never terminates, or one that uses excess amounts of memory or processor power. However, when written correctly recursion can be a very efficient and mathemaically-elegant approach to programming.
+# In this example, tri_recursion() is a function that we have defined to call itself("recurse"). We use the k variable as the data, which decrements(-1) every time we recurse. Thhe recursion ends when the condition is not greater than 0(i.e. when it is 0).
+# To a new developer it can take some time to work out hot exactly this works, best way to find out is by testing and modifying it.
+
+# Example
+# Recursion Example
+
+def tri_recursion(k):
+    if(k > 0):
+        result = k + tri_recursion(k-1)
+        print(result)
+    else:
+        result = 0
+    return result
+print("\n\nRecursion Example Results")
+tri_recursion(6)
