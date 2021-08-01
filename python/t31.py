50e34cadab1de2c8727aee9b17b0009761c7458a
HEVNz <heaven13exe@gmail.com>
2021-08-01 15:30:02 +0700

    Update t31.py

---
+++ python/t31.py
@@ -0,0 +1,12 @@
+# Python Iterators
+#
+
+# Python Iterators
+# An iterator is an object that contains a countable number of values.
+# An iterator is an object that can be iterated upon, meaning that you can traverse through all the values.
+# Technically of the methods __iter__() and __next__() .
+
+# Iterator vs Iterable
+# Lists, tuples, dictionaries, and sets are all iterable objects. They are iterable Containers which you can get an iterator from.
+# All these objects have a iter() method which is used to get an iterator:
+
