41ad60eb47031c09e895d3d14332645a2fcc7ef4
HEVNz <heaven13exe@gmail.com>
2021-08-01 15:34:56 +0700

    Update t31.py

--- python/t31.py
+++ python/t31.py
@@ -10,3 +10,37 @@
 # Lists, tuples, dictionaries, and sets are all iterable objects. They are iterable Containers which you can get an iterator from.
 # All these objects have a iter() method which is used to get an iterator:
 
+# Example
+# Return an iterator from a tuple, and print each value:
+
+mytuple = ("apple","banana","cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+# apple
+print(next(myit))
+# banana
+print(next(myit))
+# cherry
+
+# Even strings are iterable pbjects, and can return an iterator:
+
+# Example
+# Strings are also iterable objects, containing a sequence of characters:
+
+mystr = "banana"
+myit = iter(mystr)
+
+print(next(myit))
+# b
+print(next(myit))
+# a
+print(next(myit))
+# n
+print(next(myit))
+# a
+print(next(myit))
+# n
+print(next(myit))
+# a
+
