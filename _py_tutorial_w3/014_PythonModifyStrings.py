7a8ac27546ea3ca5eb8ddb3e2adacf54c8520c21
HEVNz <e-exe-e@live.com>
2021-08-10 01:53:15 +0700

    Update 014_PythonModifyStrings.py

--- _py_tutorial_w3/014_PythonModifyStrings.py
+++ _py_tutorial_w3/014_PythonModifyStrings.py
@@ -14,3 +14,54 @@
 a = "Hello, World!"
 print(a.upper())
 # HELLO, WORLD!
+
+#
+# Lower Case
+#
+
+# Example
+# The lower() method returns the string in lower case:
+
+a = "Hello, World!"
+print(a.lower())
+# hello, world!
+
+#
+# Remove Whitespace
+#
+# Whitespace is the space before and/or after the actual text, and very often you want to remove this space.
+
+# Example
+# The strip() method removes any whitespace from the begining or the end:
+
+a = "Hello, World! "
+print(a.strip())
+# Hello, World!
+
+#
+# Replace String
+#
+
+# Example
+# The replace() method replaces a string with another string():
+
+a = "Hello, World!"
+print(a.replace("H","J"))
+# Jello, World!
+
+#
+# Split String
+#
+# The split() method returns a list where the text between the specified separator becomes the list items.
+
+# Example
+# The split() method splits the string into substrings if it finds instances of the separator:
+
+a = "Hello, World!"
+print(a.split(","))
+# ['Hello', ' World!']
+
+#
+# String Methods
+#
+# Learn more about String Methods with our String Methods Reference
