99141f13253cf82923c990137fc550f710f4092e
HEVNz <e-exe-e@live.com>
2021-08-10 03:10:17 +0700

    Create 0182_PythonStringcasefoldMethod.py

---
+++ _py_tutorial_w3/0182_PythonStringcasefoldMethod.py
@@ -0,0 +1,26 @@
+##
+## Python String casefold() Method
+##
+
+# Example
+# Make the string lower casae:
+
+txt = "Hello, And Welcome To My World!"
+x = txt.casefold()
+print(x)
+# hello, and welcome to my world!
+
+#
+# Definition and Usage
+#
+# The casefold() method returns a string where all the characters are lower case.
+# This method is similar to the lower() method, but the casefold() method is stronger, more aggressice, meaning that it will convert more characters into lower case, and will find more matches when comparing two strings and both are converted using the casefold() method.
+
+#
+# Syntax
+# >> string.casefold()
+
+#
+# Parameter Values
+#
+# No parameters
