4a2972e4df4d3e46f4a831e7239fd6b8ffb5a460
HEVNz <e-exe-e@live.com>
2021-08-10 02:57:07 +0700

    Create 018_PythonStringMethods.py

---
+++ _py_tutorial_w3/018_PythonStringMethods.py
@@ -0,0 +1,53 @@
+##
+## Python - String Methods
+##
+
+#
+# String Methods
+#
+# Python has a set of built-in methods that you can use on strings.
+
+# Method            Descriptioin
+# capitalize()      Converts the first character to upper case
+# casefold()        Converts string into lower case
+# center()          Returns a centered string
+# count()           Returns the number if times a specified value occurs in a string
+# encode()          Returns an encoded version of the string
+# endwith()         Returns true if the string ends with the specified value
+# expandtabs()      Sets the tab size of the string
+# find()            Searches the string for a specified value and returns the position of where it was found
+# format()          Formats specified values in a string
+# format_map()      Formats specified values in a string
+# index()           Searches the string for a specified value and returns the position of where it was found
+# isalnum()         Returns True if all characters in the string are alphanumric
+# isalpha()         Returns True if all characters in the string are in the alphabet
+# isdecimal()       Returns True if all characters in the string are decimal
+# isdigit()         Returns True if all characters in the string are digits
+# isidentifier()    Returns True if all characters in the identifier
+# islower()         Returns True if all characters in the string are lower case
+# isnumberic()      Returns True if all characters in the string are numeric
+# isprintable()     Returns True if all characters in the string are printable
+# isspace()         Returns True if all characters in the string are whitespaces
+# istitle()         Returns True if all characters in the rules of a title
+# isupper()         Returns True if all characters in the string are upper case
+# joint()           Joins the elements of an iterable to the end of the string
+# ljust()           Returns a left justified version of the string
+# lower()           Converts a string into lower case
+# lstrip()          Returns a left trim version of the string
+# maketrans()       Returns a translation table to be used in translations
+# partition()       Returns a tuple where the string is parted into three parts
+# replace()         Returns a string where a specified value is replaced with a specified value
+# rfind()           Searches the string for a specified value and returns the last position of where it was found
+# rindex()          Searches the string for a speciified value and returns the last position of where it was found
+# rjust()           Returns a right justified version of the string
+# rpartition()      Returns a tuple where the string is parted into three parts
+# rsplit()          Splits the string at the specified separator, and returns a list
+# rstrip()          Returns a right at the specified separator, and returns a list
+# splitlines()      Splits the string at line breaks and return a list
+# startwith()       Returns true if the string starts with the specified value
+# strip()           Returns a trimmed version of the string
+# swapcase()        Swaps case, lower case becomes upper case and vice versa
+# title()           Converts the first character of each word to upper case
+# translate()       Returns a translate string
+# upper()           Converts a string into upper case
+# zfill()           Fills the string with a specified number of 0 values at the beginning
