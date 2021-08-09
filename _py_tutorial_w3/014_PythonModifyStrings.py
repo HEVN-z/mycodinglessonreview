26b81c7872f8de418b2358f140a3e22f5c1beccd
HEVNz <e-exe-e@live.com>
2021-08-10 00:58:38 +0700

    Create 014_PythonModifyStrings.py

---
+++ _py_tutorial_w3/014_PythonModifyStrings.py
@@ -0,0 +1,16 @@
+##
+## Python -Modify Strings
+##
+
+# Python has a set of built-in methods that you can use on strings.
+
+#
+# Upper Case
+#
+
+# Example
+# The upper() method returns the string in upper case:
+
+a = "Hello, World!"
+print(a.upper())
+# HELLO, WORLD!
