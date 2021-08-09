19668279f82df8dd53ecf93e59d76b987d3c8896
HEVNz <e-exe-e@live.com>
2021-08-10 03:03:53 +0700

    Create 0181_PythonStringapitalizeMethod.py

---
+++ _py_tutorial_w3/0181_PythonStringapitalizeMethod.py
@@ -0,0 +1,39 @@
+##
+## Python String capitalize() Method
+##
+
+# Example
+# Upper case the first letter in this sentence:
+
+txt = "hello, and welcome to my world."
+x = txt.capitalize()
+print(x)
+# Hello, and welcome to my world.
+
+#
+# Definition and Usage
+#
+# The capitalize() method returns a string where the first character is upper case.
+
+#
+# Syntax
+#
+# >> string.capiralize()
+#
+
+#
+# Parameter Values
+#
+# No parameters
+
+#
+# More Examples
+#
+
+# Example
+# See what happens if the first character is a number:
+
+txt = "36 is my age."
+x = txt.capitalize()
+print(x)
+# 36 is my age.
