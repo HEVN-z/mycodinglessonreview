96abcf44751baf9b3ecefd449d18d9c461b891ac
HEVNz <e-exe-e@live.com>
2021-08-11 03:32:48 +0700

    Create 01843_PythonString_upper()Method.py

---
+++ _py_tutorial_w3/01843_PythonString_upper()Method.py
@@ -0,0 +1,26 @@
+##
+## Python String upper() Method
+##
+
+# Example
+# Upper case the string:
+
+txt = "Hello my friends"
+x = txt.upper()
+print(x)
+# HELLO MY FRIENDS
+
+#
+# Definition and Usage
+#
+# The upper() method returns a string where all characters are in upper case.
+# Symbols and Numbers are ignored.
+
+#
+# Syntax
+# >> strinig.upper()
+
+#
+# Parameter Values
+# No parameters
+
