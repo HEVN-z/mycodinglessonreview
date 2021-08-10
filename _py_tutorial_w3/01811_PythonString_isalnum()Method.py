8ed558971bddaf993e875051b667baee2442378d
HEVNz <e-exe-e@live.com>
2021-08-10 14:41:03 +0700

    Create 01811_PythonString_isalnum()Method.py

---
+++ _py_tutorial_w3/01811_PythonString_isalnum()Method.py
@@ -0,0 +1,35 @@
+##
+## Python String isalnum() Method
+##
+
+# Example
+# Check if all the characters in the text are alphanumeric:
+
+txt = "Company12"
+x = txt.isalnum()
+print(x)
+# True
+
+#
+# Definition and Usage
+#
+# The isalnum() method returns True if all the characters are alphanumeric, meaning alphabet letter (a-z) and numbers (0-9).
+
+#
+# Syntax
+# >> string.isalnum()
+
+# Parameter Values
+# No parameters.
+
+#
+# More Examples
+#
+
+# Example
+# Check if all the characters in the text is alphanumeric:
+
+txt = "Company 12"
+x = txt.isalnum()
+print(x)
+# False
