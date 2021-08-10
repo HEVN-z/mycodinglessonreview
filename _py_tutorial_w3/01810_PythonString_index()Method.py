b99285930cbbc0590ed87d246ee77c9126932f86
HEVNz <e-exe-e@live.com>
2021-08-10 14:05:38 +0700

    Create 01810_PythonString_index()Method.py

---
+++ _py_tutorial_w3/01810_PythonString_index()Method.py
@@ -0,0 +1,58 @@
+##
+## Python String index() Method
+##
+
+# Example
+# Where in the text is the word "welcome"?:
+
+txt = "Hello, welcome to my world."
+x = txt.index("welcome")
+print(x)
+# 7
+
+#
+# Definition and Usage
+#
+# The index() method finds the list occurrence of the specified value.
+# The index() method raises an exception if the value is not found.
+# The index() method is almost the same as the find() method, the only difference is that the find() method returns -1 if the value is not found. (See example below)
+
+#
+# Syntax
+# >> string.index(value, start, end)
+
+# Parameter Values
+# Parameter             Description
+# value                 Required. The value to search for
+# start                 Optional. Where to start the search. Default is 0
+# end                   Optional. Where to end the search. Default is to the end of the string
+
+#
+# More Examples
+#
+
+# Example
+# Where in the text is the first occurrence of the letter "e"?:
+
+txt = "Hello, welcome to my world."
+x = txt.index("e")
+print(x)
+# 1
+
+# Example
+# Where in the text is the first occurrence of the letter "e" when you only search between position 5 and 10?:
+
+txt = "Hello, welcome to my world."
+x = txt.index("e", 5, 10)
+print(x)
+# 8
+
+# Example
+# If the value is not found, the find() method returns -1, but the index() method will raise an exception:
+
+txt = "Hello, welcome to my world."
+print(txt.find("q"))
+# -1
+# print(txt.index("q"))
+# ValueError: substring not found
+
