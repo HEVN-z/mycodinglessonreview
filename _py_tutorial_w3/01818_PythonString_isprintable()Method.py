bd976c7fe568ed1751719ec35bc85a2a55254725
HEVNz <e-exe-e@live.com>
2021-08-10 15:30:16 +0700

    Create 01818_PythonString_isprintable()Method.py

---
+++ _py_tutorial_w3/01818_PythonString_isprintable()Method.py
@@ -0,0 +1,35 @@
+##
+## Python String isprintable() Method
+##
+
+# Example
+# Check if all the characters in the text are printable:
+
+txt = "Hello! Are you #1?"
+x = txt.isprintable()
+print(x)
+# True
+
+#
+# Definition and Usage
+#
+# The isprintable() method returns True if all the characters are printable, otherwise False.
+
+#
+# Syntax
+# >> string.isprintable()
+
+# Parameter Values
+# No parameters.
+
+#
+# More Examples
+#
+
+# Example
+# Check if all the characters in the text are printable:
+
+txt = "Hello!\nAre you #1?"
+x = txt.isprintable()
+print(x)
+# False
