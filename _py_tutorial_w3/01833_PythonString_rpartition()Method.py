480c83ffa5a7c5552fd36372d928d0c77d931f7e
HEVNz <e-exe-e@live.com>
2021-08-11 01:44:13 +0700

    Create 01833_PythonString_rpartition()Method.py

---
+++ _py_tutorial_w3/01833_PythonString_rpartition()Method.py
@@ -0,0 +1,44 @@
+##
+## Python String rpartition() Method
+##
+
+# Example
+# Search for the last occurrence of the word "bananas", and return a tuple with three elements:
+# 1 - everything before the "match"
+# 2 - the "match"
+# 3 - everything after the "march"
+
+txt = "I could eat bananas all day, bananas are my favorite fruit"
+x = txt.rpartition("bananas")
+print(x)
+# ('I could eat bananas all day, ', 'bananas', ' are my favorite fruit')
+
+#
+# Definition and Usage
+#
+# The rpartition() method searches for the last occurrence of a specified string, and splits into a tuple containing three elements.
+# The first element contains the part before the specified string.
+# The second element contains the specified string.
+# The third element contains the parat after the string.
+
+#
+# Syntax
+# >> string.rpartition(value)
+
+#
+# Parameter Values
+
+# Parameter     Description
+# value         Required. The string to search for
+
+#
+# More Examples
+#
+
+# Example
+# If the specified value is not found, the rpartition() method returns a tuple containing: 1 - an empty string, 2 - an empty string, 3 - the whole string:
+
+txt = "I could eat bananas all day, bananas are my favorite fruit"
+x = txt.rpartition("apples")
+print(x)
+# ('', '', 'I could eat bananas all day, bananas are my favorite fruit')
