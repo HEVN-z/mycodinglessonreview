20e266c1ff4e1ea35663daa33bc5163aa203ca9b
HEVNz <e-exe-e@live.com>
2021-08-11 03:16:02 +0700

    Create 01841_PythonString_title()Method.py

---
+++ _py_tutorial_w3/01841_PythonString_title()Method.py
@@ -0,0 +1,45 @@
+##
+## Python String title() Method
+##
+
+# Example
+# Make the first letter in each word upper case:
+
+txt = "Welcome to my world"
+x = txt.title()
+print(x)
+# Welcome To My World
+
+#
+# Definition and Usage
+#
+# The title() method returns a string where the first character in every word is upper case. Like a header, or a title.
+# If the word contains a number or a symbol, the first letter after that will be converted to upper case.
+
+#
+# Syntax
+# >> string.title()
+
+#
+# Parameter Values
+# No parameters.
+
+#
+# More Examples
+#
+
+# Example
+# Make the first letter in each word upper case:
+
+txt = "Welcome to my 2nd world"
+x = txt.title()
+print(x)
+# Welcome To My 2Nd World
+
+# Example
+# Note that the first letter after a non-alphabet letter is converted into a upper case letter:
+
+txt = "hello b2b2b2 and 3g3g3g"
+x = txt.title()
+print(x)
+# Hello B2B2B2 And 3G3G3G
