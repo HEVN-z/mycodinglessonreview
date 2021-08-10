9e9ea9b2a7db311dfa2a882c22a22b34042699ad
HEVNz <e-exe-e@live.com>
2021-08-11 03:09:44 +0700

    Create 01840_PythonString_swapcase()Method.py

---
+++ _py_tutorial_w3/01840_PythonString_swapcase()Method.py
@@ -0,0 +1,25 @@
+##
+## Python String swapcase() Method
+##
+
+# Example
+# Make the lower case letters upper case and the upper case letters lower case:
+
+txt = "Hello My Name Is PETER"
+x = txt.swapcase()
+print(x)
+# hELLO mY nAME iS peter
+
+#
+# Definition and Usage
+#
+# The swapcase() method returns a string where all the upper case letters are lower case and vice versa,
+
+#
+# Syntax
+# >> string.swapcase()
+
+#
+# Parameter Values
+# No parameters.
+
