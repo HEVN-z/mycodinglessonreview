1012a69c135f9dcbe3b21f284163a21040ed65d8
HEVNz <e-exe-e@live.com>
2021-08-11 13:14:47 +0700

    Create 020_PythonOperators.py

---
+++ _py_tutorial_w3/020_PythonOperators.py
@@ -0,0 +1,116 @@
+##
+## Python Operators
+##
+
+#
+# Python Operators
+#
+# Operators are uused to perform operators on variables and values.
+# In the example below, we use thhe + operator to add together two values:
+
+# Example
+
+print(10 + 5)
+# 15
+
+# Python devides the operators in the following groups:
+#  ◼ Arithmetic operators
+#  ◼ Assignment operators
+#  ◼ Comparison operators
+#  ◼ Logical operators
+#  ◼ Identity operators
+#  ◼ Membership operators
+#  ◼ Bitwise operators
+
+#
+# Python Arithmetic Operators
+#
+# Arithmetoc operators are used woth numeric values to perform common mathematical operations:
+
+# Operator  Name            Example
+# +         Addition        x + y
+# -         Subtraction     x - y
+# *         Multiplication  x * y
+# /         Division        x / y
+# %         Modulus         x % y
+# **        Exponentiation  x ** y
+# //        Floor division  x // y
+
+#
+# Python Assignment Operators
+#
+# Assignment operator are used to assign values to variables:
+
+# Operator  Example         Same As
+#   =       x = 5           x = 5
+#   +=      x += 3          x = x + 3
+#   -=      x -= 3          x = x - 3
+#   *=      x *= 3          x = x * 3
+#   /=      x /= 3          x = x / 3
+#   %=      x %= 3          x = x % 3
+#   //=     x //= 3         x = x // 3
+#   **=     x **= 3         x = x ** 3
+#   &=      x &= 3          x = x & 3
+#   |=      x |= 3          x = x | 3
+#   ^=      x ^= 3          x = x ^ 3
+#   >>=     x >>= 3         x = x >> 3
+#   <<=     x <<= 3         x = x << 3
+
+#
+# Python Comparison Operators
+#
+# Comparison operators are used to compare two values:
+
+# Operator  Name                        Example
+#   ==      Equal                       x == y
+#   !=      Not equal                   x != y
+#   >       Greater than                x > y
+#   <       Less than                   x < y
+#   >=      Greater than or equal to    x >= y
+#   <=      Less than or equal to       x <= y
+
+#
+# Python Logical Operatos
+#
+# Logical operators are used to combine conditional statements:
+
+# Operator  Description                                                 Example
+# and       Return True if both statements are true                     x < 5 and x < 10
+# or        Returns True if one of the statements is true               x < 5 or x < 4
+# not       Reverse the result, returns False if the result is true     not(x < 5 and x < 10)
+
+#
+# Python Identity Operators
+#
+# Identity operators are used to compare the objects, not if they are equal, but if they are acturlly the same object, with the same memory location:
+
+# Operator  Description                                                 Example
+#   is      Returns True if both variables are the same object          x is y
+#   is not  Returns True if both variables are not the same object      x is not y
+
+#
+# Python Membership Operators
+#
+# Membership operators are used to test if a sequence is presented in an object:
+
+# Operator  Description                                                 Example
+#   in      Returns True if a sequence with the specified value is      x in y
+#           present in the object
+#   not in  Returns True if a sequence with the specified value is      x not in y
+#           NOT present in the object
+
+#
+# Python Bitwise Operators
+#
+# Bitwise operators are used to compare (binary) bumbers:
+
+# Operator      Name            Description
+#   &           AND             Sets each bit to 1 if both bits are 1
+#   |           OR              Sets each bit to 1 if one of two bits is 1
+#   ^           XOR             Sets each bit to 1 if only one of two bits is 1
+#   ~           NOT             Inverts all the bits
+#   <<          Zero fill       Shift left by pushing zeros in from the right and let
+#               left shift      the leftmost bits fall off
+#   >>          Signed          Shift right by pushing copies of the leftmost bit in
+#               right shift     from the left, and let the rightmost bits fall off
+
