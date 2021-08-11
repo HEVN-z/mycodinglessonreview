e2ebde1a44ee4514d8009dcb996915a7102d2ceb
HEVNz <e-exe-e@live.com>
2021-08-11 14:12:59 +0700

    Create 021_03_PythonAddListItems.py

---
+++ _py_tutorial_w3/021_03_PythonAddListItems.py
@@ -0,0 +1,17 @@
+##
+## Python - Add List Items
+##
+
+#
+# Append Items
+#
+# OTo add an item to end of the list, use the append() method:
+
+# Example
+# Using the append() method to append an item:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+# ['apple', 'banana', 'cherry', 'orange']
+
