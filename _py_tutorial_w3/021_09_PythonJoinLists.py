6909fd6d506b010f6ac4983d5436397691a45426
HEVNz <e-exe-e@live.com>
2021-08-13 23:49:38 +0700

    Create 021_09_PythonJoinLists.py

---
+++ _py_tutorial_w3/021_09_PythonJoinLists.py
@@ -0,0 +1,34 @@
+##
+## Python - Join Lists
+##
+
+#
+# Join Two Lists
+#
+# There are several ways to join, or concatenate, two or more lists in Python.
+# One of the easiest ways are by using the + operator.
+
+# Example
+# Join two list:
+
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+list3 = list1 + list2
+print(list3)
+# ['a', 'b', 'c', 1, 2, 3]
+list3 = list2 + list1
+print(list3)
+# [1, 2, 3, 'a', 'b', 'c']
+
+# Another way to join two lists is by appending all the items from list2 into list1, one by one:
+
+# Example
+# Append list2 into list1:
+
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+for x in list2:
+    list1.append(x)
+print(list1)
+# ['a', 'b', 'c', 1, 2, 3]
+
